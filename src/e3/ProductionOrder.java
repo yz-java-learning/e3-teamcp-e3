a751895cddd146a88d1ff020cea50be797e10df4
Charles P <petchsycp@gmail.com>
2017-06-28 13:15:59 -0400

    Updated "update", "display", and "toString"

--- src/e3/ProductionOrder.java
+++ src/e3/ProductionOrder.java
@@ -13,14 +13,24 @@ public class ProductionOrder implements Observer, DisplayElement {
 	}
 	
 	public void update(double availQty, double ordQty) {
-		
+		this.minQuantity = 1000.00;
+		if (ordQty < this.minQuantity){
+			/* "there is a minimum amount of wallpaper that can be 
+			 * printed in one production run (1000 yards for most
+			 * common types of commercial wallpaper)." - e3.pdf
+			 */
+			ordQty = 0.0;
+		}
+		// Please check if this is correct.
+		this.minQuantity = ordQty;
 	}
 	
 	public void display(double dispQty) {
-		
+		System.out.println("Quantity: " + dispQty);
 	}
 	
 	public String toString() {
-		return "";
+		return ("Minimum Quantity: " + this.minQuantity + ", " +  
+				"Inventory: " + this.inventory);
 	}
 }
