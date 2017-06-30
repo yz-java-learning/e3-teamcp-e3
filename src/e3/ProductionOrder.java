6811ad23335542e7aac1e9af7a5e5a91b12cc6a9
yichen <yichenzhu1337@gmail.com>
2017-06-29 23:33:50 -0400

    auto increment on orderSequence

--- src/e3/ProductionOrder.java
+++ src/e3/ProductionOrder.java
@@ -2,29 +2,42 @@ package e3;
 
 public class ProductionOrder implements Observer, DisplayElement {
 
-	private int orderSequence; // Isn't this supposed to be static?
-	protected int ID;
-	protected double minQuantity;
-	protected Observable inventory;
-	
-	public ProductionOrder(int minQty, Observable inventory) {
-		this.minQuantity = minQty;
-		this.inventory = inventory;		
-	}
-	
-	public void update(double availQty, double ordQty) {
-		if(ordQty >= this.minQuantity){
-			Inventory inventory = (Inventory) this.inventory;
-			inventory.updateQuantities(ordQty, 0);
-		}
-	}
-	
-	public void display(double dispQty) {
-		System.out.println("Quantity: " + dispQty);
-	}
-	
-	public String toString() {
-		return ("Minimum Quantity: " + this.minQuantity + ", " +  
-				"Inventory: " + this.inventory);
-	}
+    private static int orderSequence;
+    protected int ID;
+    protected double minQuantity;
+    protected Observable inventory;
+
+    public ProductionOrder(int minQty, Observable inventory) {
+        this.minQuantity = minQty;
+        this.inventory = inventory;
+
+        // auto increment by one each time we make a ProductionOrder
+        ID = ++orderSequence;
+
+        Inventory currentInventory = (Inventory) this.inventory;
+        currentInventory.registerObserver(this);
+    }
+
+    /**
+     * If the availableQuantity in our inventory is greater than or equal to
+     * the quantity we ordered then we update the inventories quantities
+     *
+     * @param availQty Available quantity in our inventory
+     * @param ordQty   Order quantity we make
+     */
+    public void update(double availQty, double ordQty) {
+        if (ordQty >= this.minQuantity) {
+            Inventory currentInventory = (Inventory) this.inventory;
+            currentInventory.updateQuantities(ordQty, 0);
+        }
+    }
+
+    public void display(double dispQty) {
+        System.out.println("Quantity: " + dispQty);
+    }
+
+    public String toString() {
+        return ("Minimum Quantity: " + this.minQuantity + ", " +
+                "Inventory: " + this.inventory);
+    }
 }
